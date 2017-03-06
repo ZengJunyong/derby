5e48992befcc868ff6c2de6779a412a9c22be6a0
yong <zengjunyong@gmail.com>
2017-03-06 16:34:07 +0800

    01.sql

---
+++ 01.sql
@@ -0,0 +1,3 @@
+insert into derbyDB values (1956,'Webster St.');
+update derbyDB set num=180, addr='Grand Ave.' where num=1956;
+select * from derbyDb;
---
+++ README.md
@@ -0,0 +1,3 @@
+Derby 使用的2种方式：内嵌和独立
+http://blog.csdn.net/remote_roamer/article/details/4830723
+
